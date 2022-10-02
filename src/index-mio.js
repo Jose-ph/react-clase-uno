fe900c3a1f84cc4c52244e716d5ba79081753583
Jose-ph <68978572+Jose-ph@users.noreply.github.com>
2022-10-02 11:35:02 -0300

    resuelve ejercicio tarjeta

---
+++ src/index-mio.js
@@ -0,0 +1,33 @@
+import { Welcome } from './components/Welcome.js';
+import {Tarjeta} from'./tarea/Tarea1.js'
+
+// ReactDOM.render(<Welcome nombre="Julián" />, document.getElementById('react-app'));
+ReactDOM.render(<Tarjeta nombre="Jose Basualdo" titulo="FullStack Developer" imagen="https://avatars.githubusercontent.com/u/68978572?v=4" />, document.getElementById('react-app'));
+
+// import { Button } from './components/Button.js';
+
+// ReactDOM.render(
+//   <Button onClick={event => alert(event.target.value)}>Hacé click!</Button>,
+//   document.getElementById('react-app')
+// );
+
+// import { Page } from './components/Page.js';
+
+// ReactDOM.render(
+//   <Page
+//     titulo="Mi diario"
+//     articulos={[
+//       { titulo: 'Dia uno', cuerpo: 'Hoy vi una ardilla.' },
+//       { titulo: 'Dia dos', cuerpo: 'Hoy vi otra ardilla, pero capaz sea la misma.' },
+//     ]}
+//   />,
+//   document.getElementById('react-app')
+// );
+
+// import { LikeButton } from './components/LikeButton.js';
+
+// ReactDOM.render(<LikeButton />, document.getElementById('react-app'));
+
+// import { ToDoList } from './components/ToDoList.js';
+
+// ReactDOM.render(<ToDoList />, document.getElementById('react-app'));
